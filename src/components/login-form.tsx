d2568c67972e78d9aa3619dd6c0c465e9c1bd190
jlp <mattlukin@gmail.com>
2023-07-05 17:26:11 +0100

    added dotenv and firebase

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react"; //to define a component
+import {getApp} from "firebase/app"
 
 export const LoginForm: FC = () => {
-    return <h1>hello login form</h1>
+    return <h1>{JSON.stringify(getApp())}</h1>
 } //FC type - functional component
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
+
+initializeApp(firebaseConfig)
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
