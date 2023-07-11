446bcb798a9f8221d555747ca12881d263ec134a
jlp <mattlukin@gmail.com>
2023-07-11 13:49:28 +0100

    upload and delete models in firebase from the Model List

--- src/components/building/front-menu/front-menu-content/model-list-menu.tsx
+++ src/components/building/front-menu/front-menu-content/model-list-menu.tsx
@@ -1,5 +1,76 @@
 import { FC } from "react";
+import { useAppContext } from "../../../../middleware/context-provider";
+import "./front-menu-context.css";
+import { IconButton, Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Clear";
 
 export const ModelListMenu: FC = () => {
-    return <p>This is the model list!</p>
-}
+  const [{ building, user }, dispatch] = useAppContext();
+
+  if (!building || !user) {
+    throw new Error("Error: building not found!");
+  }
+
+  const onUploadModel = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.style.visibility = "hidden";
+    document.body.appendChild(input);
+
+    input.onchange = () => {
+      console.log(input.files);
+
+      if (input.files && input.files.length) {
+        const file = input.files[0]; //just get the first file
+        const newBuilding = { ...building }; //copy of the building object
+        const { name } = file;
+        const id = `${name}-${performance.now()}`; //id of the model
+        const model = {name, id };
+        newBuilding.models.push(model);
+        console.log(newBuilding)
+        dispatch({ type: "UPDATE_BUILDING", payload: newBuilding });
+        dispatch({
+          type: "UPLOAD_MODEL",
+          payload: {
+            model,
+            file,
+            building: newBuilding,
+          },
+        });
+      }
+
+      input.remove(); //cleanup
+    };
+
+    input.click();
+  };
+
+  const onDeleteModel = (id: string) => {
+    const newBuilding = {...building}
+    const model = newBuilding.models.find(model => model.id === id)
+    if (!model) throw new Error("Model not found!")
+    newBuilding.models = newBuilding.models.filter((model) => model.id !== id)
+    dispatch({ type: "UPDATE_BUILDING", payload: newBuilding });
+    dispatch({type: "DELETE_MODEL", payload: {model, newBuilding}})
+  }
+
+  return (
+    <div>
+      {building.models.length ? (
+        building.models.map((model) => (
+          <div className="list-item" key={model.id}>
+            <IconButton onClick={() => onDeleteModel(model.id)}>
+              <DeleteIcon />
+            </IconButton>
+            <span className="margin-left">{model.name}</span>
+          </div>
+        ))
+      ) : (
+        <p>This building has no models!</p>
+      )}
+      <div className="list-item">
+        <Button onClick={onUploadModel}>Upload model</Button>
+      </div>
+    </div>
+  );
+};
--- src/core/db/db-handler.ts
+++ src/core/db/db-handler.ts
@@ -1,55 +1,60 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { Building, Model } from "../../types";
 import { Events } from "../../middleware/event-handler";
 import { getFirestore, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { getApp } from "firebase/app";
 //import { Action } from "../middleware/actions";
-import {deleteObject, getStorage, ref, uploadBytes} from "firebase/storage"
+import { deleteObject, getStorage, ref, uploadBytes } from "firebase/storage";
 
 export const databaseHandler = {
-    login: () => {
-       
-        const auth = getAuth();
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider);
+  login: () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  },
 
-    },
+  logout: () => {
+    const auth = getAuth();
+    signOut(auth);
+  },
 
-    logout: () => {
-        const auth = getAuth();
-        signOut(auth);
-    },
+  deleteBuilding: async (building: Building, events: Events) => {
+    //const id = building.uid;
+    const dbInstance = getFirestore(getApp());
+    await deleteDoc(doc(dbInstance, "buildings", building.uid));
+    events.trigger({ type: "CLOSE_BUILDING" });
+  },
 
-    deleteBuilding: async (building:Building, events: Events) => {
-        //const id = building.uid;
-        const dbInstance = getFirestore(getApp())
-        await deleteDoc(doc(dbInstance, "buildings", building.uid))
-        events.trigger({type: "CLOSE_BUILDING"})
+  updateBuilding: async (building: Building) => {
+    const dbInstance = getFirestore(getApp());
+    await updateDoc(doc(dbInstance, "buildings", building.uid), {
+      ...building,
+    });
+  },
 
+  uploadModel: async (
+    model: Model,
+    file: File,
+    building: Building,
+    events: Events
+  ) => {
+    const appInstance = getApp();
+    const storageInstance = getStorage(appInstance);
+    const fileRef = ref(storageInstance, model.id);
+    await uploadBytes(fileRef, file);
+    events.trigger({ type: "UPDATE_BUILDING", payload: building });
+  },
 
-    },
-
-    updateBuilding: async (building:Building) => {
-        const dbInstance = getFirestore(getApp())
-        await updateDoc(doc(dbInstance, "buildings", building.uid), {
-            ...building,
-        })
-    },
-
-    uploadModel: async (model: Model, file: File, building: Building, events: Events) => {
-        const appInstance = getApp();
-        const storageInstance = getStorage(appInstance);
-        const fileRef = ref(storageInstance, model.id);
-        await uploadBytes(fileRef, file);
-        events.trigger({type: "UPDATE_BUILDING", payload: building})
-    },
-
-    deleteModel: async (model: Model, building: Building, events: Events) => {
-        const appInstance = getApp();
-        const storageInstance = getStorage(appInstance);
-        const fileRef = ref(storageInstance, model.id);
-        await deleteObject(fileRef);
-        events.trigger({type: "DELETE_BUILDING", payload: building})
-    }
-
-}
+  deleteModel: async (model: Model, building: Building, events: Events) => {
+    const appInstance = getApp();
+    const storageInstance = getStorage(appInstance);
+    const fileRef = ref(storageInstance, model.id);
+    await deleteObject(fileRef);
+    events.trigger({ type: "UPDATE_BUILDING", payload: building });
+  },
+};
