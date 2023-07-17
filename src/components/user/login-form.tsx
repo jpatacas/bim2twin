54196055fc3117b6b7ae4ce7698054f691d7ae6f
jlp <mattlukin@gmail.com>
2023-07-17 17:12:57 +0100

    improved login page styling

--- src/components/user/login-form.tsx
+++ src/components/user/login-form.tsx
@@ -1,41 +1,159 @@
-import { FC } from "react"; //to define a component
+// import { FC } from "react"; //to define a component
+// import { Navigate } from "react-router-dom";
+// import { Box, Button, Card, CardContent, TextField, Typography } from "@mui/material";
+// import { useAppContext } from "../../middleware/context-provider";
+// import "./user-styles.css";
+// //import {getApp} from "firebase/app"
+
+// export const LoginForm: FC = () => {
+//   const [state, dispatch] = useAppContext();
+
+//   const onLogin = () => {
+//    // console.log("Logging in!");
+//     dispatch({type: "LOGIN"});
+
+//   };
+
+//   if (state.user) {
+//     return <Navigate to="/map"/>
+//   }
+
+//   return (
+
+//     <Card sx={{ maxWidth: 400, margin: "0 auto", marginTop: 30 }}>
+//     <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+//       <img className="landing-logo" alt="ifcjs logo" src="ifcjs-logo.png" />
+
+//       <Typography variant="h5" component="h1" align="center" gutterBottom>
+//         Login
+//       </Typography>
+
+//       <TextField
+//         label="Email"
+//         type="email"
+//         variant="outlined"
+//         margin="normal"
+//         fullWidth
+//         // Add necessary props and event handlers for email input
+//       />
+
+//       <TextField
+//         label="Password"
+//         type="password"
+//         variant="outlined"
+//         margin="normal"
+//         fullWidth
+//         // Add necessary props and event handlers for password input
+//       />
+
+//       <Button variant="contained" color="primary" onClick={onLogin} fullWidth>
+//         Login
+//       </Button>
+//     </CardContent>
+//   </Card>
+
+//   );
+// }; //FC type - functional component
+
+import { FC, useState } from "react";
 import { Navigate } from "react-router-dom";
-import { Box, Button  } from "@mui/material";
+import { Box, Button, Card, CardContent, TextField, Typography, Tab, Tabs } from "@mui/material";
 import { useAppContext } from "../../middleware/context-provider";
 import "./user-styles.css";
-//import {getApp} from "firebase/app"
 
 export const LoginForm: FC = () => {
   const [state, dispatch] = useAppContext();
+  const [activeTab, setActiveTab] = useState(0);
 
   const onLogin = () => {
-   // console.log("Logging in!");
-    dispatch({type: "LOGIN"});
+    dispatch({ type: "LOGIN" });
+  };
 
+  const onSignUp = () => {
+   // dispatch({ type: "SIGNUP" });
+   console.log("Sign up")
+  };
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
   };
 
   if (state.user) {
-    return <Navigate to="/map"/>
+    return <Navigate to="/map" />;
   }
 
   return (
-    <Box
-    sx={{
-      marginTop: 8,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    }}
-  >
-    <img className="landing-logo" alt="ifcjs logo" src="ifcjs-logo.png" />
+    <Card sx={{ maxWidth: 400, margin: "0 auto", marginTop: 20 }}>
+      <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h1>BIM2TWIN</h1>
+        <img className="landing-logo" alt="ifcjs logo" src="ifcjs-logo.png" />
 
-    <Button variant="outlined" onClick={onLogin}>
-      Login
-    </Button>
-  </Box>
+        <Tabs value={activeTab} onChange={handleTabChange} centered>
+          <Tab label="Login" />
+          <Tab label="Sign Up" />
+        </Tabs>
 
-    // <h1>
-    //     <Button variant= "outlined"  onClick={onLogin}>Login</Button>
-    // </h1>
+        {activeTab === 0 && (
+          <>
+            <TextField
+              label="Email"
+              type="email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              // Add necessary props and event handlers for email input
+            />
+
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              // Add necessary props and event handlers for password input
+            />
+            <Box sx={{ width: "100%", marginTop: 2}}>
+            <Button variant="contained" color="primary" onClick={onLogin} fullWidth>
+              Login
+            </Button>
+            </Box>
+
+            <Box sx={{ width: "100%", marginTop: 2}}>
+            <Button variant="contained" color="secondary" onClick={onLogin} fullWidth>
+              Login with Google
+            </Button>
+            </Box>
+          </>
+        )}
+
+        {activeTab === 1 && (
+          <>
+            {/* Add sign-up form fields and button */}
+            <TextField
+              label="Email"
+              type="email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              // Add necessary props and event handlers for email input
+            />
+
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              // Add necessary props and event handlers for password input
+            />
+            <Box sx={{ width: "100%", marginTop: 2}}>
+            <Button variant="contained" color="primary" onClick={onSignUp} fullWidth>
+              Sign Up
+            </Button>
+            </Box>
+          </>
+        )}
+      </CardContent>
+    </Card>
   );
-}; //FC type - functional component
+};
