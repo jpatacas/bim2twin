f813b35bffc57d0083a444ba74ffcbae7028614a
jlp <mattlukin@gmail.com>
2023-08-23 18:45:58 +0100

    cleanup code in EnergyMenu

--- src/components/building/front-menu/front-menu-content/energy-menu.tsx
+++ src/components/building/front-menu/front-menu-content/energy-menu.tsx
@@ -8,63 +8,9 @@ import { useAppContext } from "../../../../middleware/context-provider";
 import { databaseHandler } from "../../../../core/db/db-handler";
 import { EnergyData } from "../../../../types";
 
-// 1.energy consumption plot needs all data sources (elec grid, gas, wind, solar) and stack them (e.g bar chart stack)
-// 2. solar and wind generation graphs
-// 3. overall costs stacked (elec grid + gas)
-
-// Mock data for energy use by month (replace with actual data)
-// const gasData = [120, 150, 130, 140, 90, 80, 70, 90, 120, 140, 160, 150];
-// const electricityData = [
-//   200, 250, 230, 220, 240, 260, 250, 270, 280, 260, 240, 230,
-// ];
-// const solarData = [50, 60, 70, 70, 80, 90, 100, 90, 80, 70, 60, 50];
-// const windData = [80, 80, 90, 60, 70, 70, 90, 80, 100, 80, 90, 80];
-
 export const EnergyMenu: FC = () => {
-  const [state, dispatch] = useAppContext();
+  const [state] = useAppContext();
   const [energyData, setEnergyData] = useState<EnergyData[]>([]) 
-
-  //useEffect to add mock data
-  // useEffect(() => {
-  //   console.log("useEffect running");
-
-  //   const buildingId = "clb1tGh504gYrYgPhf0g";
-
-  //   // Fetch energy data from the database for the specified building
-  //   dispatch({ type: "GET_ENERGY_DATA", payload: buildingId });
-    
-
-  //   // Array of month names
-  //   const months = [
-  //     "January",
-  //     "February",
-  //     "March",
-  //     "April",
-  //     "May",
-  //     "June",
-  //     "July",
-  //     "August",
-  //     "September",
-  //     "October",
-  //     "November",
-  //     "December",
-  //   ];
-
-  //   // Create mock energy data entries for each month
-  //   const mockEnergyData = months.map((month, index) => ({
-  //     buildingId,
-  //     month,
-  //     electricity: electricityData[index],
-  //     gas: gasData[index],
-  //     solar: solarData[index],
-  //     wind: windData[index],
-  //   }));
-
-  //   //Dispatch the ADD_ENERGY_DATA action for each entry
-  //   mockEnergyData.forEach((entry) => {
-  //     dispatch({ type: "ADD_ENERGY_DATA", payload: entry });
-  //   });
-  // }, []);
 
   useEffect(() => {
     // Fetch energy data for a specific building from the database
